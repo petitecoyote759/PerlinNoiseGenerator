e6b9587d8392553a997272d9a627d7de5f76e060
Mattie <matthew.short.nh@gmail.com>
2026-01-04 16:18:50 +0000

    Added seed support and trees and resources

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ namespace ShortTools.Perlin
 {
     public static class Perlin
     {
-        public static float[,] GeneratePerlinMap(int width, int height, int gridSize, float scale = 1f)
+        public static float[,] GeneratePerlinMap(int width, int height, int gridSize, float scale = 1f, Random? random = null)
         {
             float[,] map = new float[width, height];
 
-            Vector2[,] vectorField = GenVectorField((width / gridSize) + 2, (height / gridSize) + 2);
+            Vector2[,] vectorField = GenVectorField((width / gridSize) + 2, (height / gridSize) + 2, random);
 
             for (int x = 0; x < width; x++)
             {
@@ -64,10 +64,10 @@ namespace ShortTools.Perlin
         }
 
 
-        private static Vector2[,] GenVectorField(int width, int height)
+        private static Vector2[,] GenVectorField(int width, int height, Random? random)
         {
             Vector2[,] field = new Vector2[width, height];
-            Random random = new Random();
+            random ??= new Random();
 
             for (int x = 0; x < width; x++)
             {
@@ -114,12 +114,67 @@ namespace ShortTools.Perlin
     internal static class PerlinTester
     {
         static GraphicsHandler renderer;
-        static float[,]? PerlinMap = null;
-        const int scale = 1;
+        static float[,]? perlinMap = null;
+        static float[,]? treeMap = null;
+        static List<ResourceMap> resourceMaps = new List<ResourceMap>();
 
+        internal struct ResourceMap
+        {
+            public string name;
+            public float[,] map;
+            public int minDist;
+            public float minValue;
+            public Tuple<byte, byte, byte> colour;
+            public ResourceMap(string name, float[,] map, int minDist, Tuple<byte, byte, byte> colour, float minValue = 0.1f)
+            {
+                this.name = name; this.map = map; this.minDist = minDist; this.minValue = minValue; this.colour = colour;
+            }
+        }
+
+
+        const int scale = 2;
+        static Tuple<int, int> centre = new Tuple<int, int>(0,0);
+        const bool askForSeed = true;
 
         private static void Main()
         {
+            int seed;
+            while (askForSeed)
+            {
+                Console.WriteLine($"Autogen seed? [Y/N]");
+                string option = (Console.ReadLine() ?? "").ToUpperInvariant();
+                if (option == "Y")
+                {
+                    seed = new Random().Next(int.MinValue, int.MaxValue);
+                    break;
+                }
+                else if (option == "N")
+                {
+                    Console.Write("Seed: ");
+                    if (int.TryParse(Console.ReadLine() ?? "", out int result))
+                    {
+                        seed = result;
+                        Console.Write('\n');
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"That is not a number!\n\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Not a valid input! Please input either Y or N.\n\n");
+                }
+            }
+
+            Random random = new Random(seed);
+            Console.WriteLine($"Seed: {seed}");
+
+
+
+
+
             using (renderer = new GraphicsHandler(
                 render: Render,
                 flags: [RendererFlag.OutputToTerminal, RendererFlag.HalfSizedWindow]))
@@ -129,7 +184,19 @@ namespace ShortTools.Perlin
 
                 renderer.Pause();
 
-                tempCreateMap(renderer.screenwidth, renderer.screenheight);
+                centre = new Tuple<int, int>(renderer.screenwidth / 2, renderer.screenheight / 2);
+                treeMap = Perlin.GeneratePerlinMap(renderer.screenwidth / scale, renderer.screenheight / scale, 8, random: random);
+                resourceMaps.Add(
+                    new ResourceMap("Glucryte", Perlin.GeneratePerlinMap(renderer.screenwidth / scale, renderer.screenheight / scale, 4, random: random), 
+                    0, new Tuple<byte, byte, byte>(0, 255, 100), 0.4f));
+                resourceMaps.Add(
+                    new ResourceMap("Fructate", Perlin.GeneratePerlinMap(renderer.screenwidth / scale, renderer.screenheight / scale, 6, random: random), 
+                    100, new Tuple<byte, byte, byte>(100, 255, 50), 0.2f));
+                resourceMaps.Add(
+                    new ResourceMap("Galactyte", Perlin.GeneratePerlinMap(renderer.screenwidth / scale, renderer.screenheight / scale, 4, random: random), 
+                    200, new Tuple<byte, byte, byte>(150, 100, 200), 0.5f));
+
+                tempCreateMap(renderer.screenwidth, renderer.screenheight, random);
 
                 renderer.Resume();
                 
@@ -141,18 +208,18 @@ namespace ShortTools.Perlin
             }
         }
 
-        static void tempCreateMap(int width, int height)
+        static void tempCreateMap(int width, int height, Random random)
         {
-            float[,] continentMap = Perlin.GeneratePerlinMap(width / scale, height / scale, 64, 4f);
-            float[,] firstMap = Perlin.GeneratePerlinMap(width / scale, height / scale, 16);
-            float[,] secondMap = Perlin.GeneratePerlinMap(width / scale, height / scale, 8, 0.25f);
+            float[,] continentMap = Perlin.GeneratePerlinMap(width / scale, height / scale, 64, 4f, random: random);
+            float[,] firstMap = Perlin.GeneratePerlinMap(width / scale, height / scale, 16, random: random);
+            float[,] secondMap = Perlin.GeneratePerlinMap(width / scale, height / scale, 8, 0.25f, random: random);
 
-            PerlinMap = Perlin.CombineFloatMaps(firstMap, secondMap, 1.25f);
-            PerlinMap = Perlin.CombineFloatMaps(continentMap, PerlinMap, 5f);
+            perlinMap = Perlin.CombineFloatMaps(firstMap, secondMap, 1.25f);
+            perlinMap = Perlin.CombineFloatMaps(continentMap, perlinMap, 5f);
 
-            PerlinMap = ApplyFuncToMap(PerlinMap);
+            perlinMap = ApplyFuncToMap(perlinMap);
 
-            PerlinMap = CreateIsland(PerlinMap);
+            perlinMap = CreateIsland(perlinMap);
         }
 
 
@@ -183,13 +250,13 @@ namespace ShortTools.Perlin
         const bool coloured = true;
         private static void Render()
         {
-            if (PerlinMap is null) { return; }
+            if (perlinMap is null) { return; }
 
             // perlin map goes from -1 to 1, so to turn to black and white we can do
             // (x + 1) * 255 / 2
-            for (int x = 0; x < PerlinMap.GetLength(0); x++)
+            for (int x = 0; x < perlinMap.GetLength(0); x++)
             {
-                for (int y = 0; y < PerlinMap.GetLength(1); y++)
+                for (int y = 0; y < perlinMap.GetLength(1); y++)
                 {
                     byte r;
                     byte g;
@@ -197,12 +264,12 @@ namespace ShortTools.Perlin
 
                     if (coloured)
                     {
-                        Tuple<byte, byte, byte> colours = GetColours(PerlinMap[x, y]);
+                        Tuple<byte, byte, byte> colours = GetColours(x, y, perlinMap);
                         r = colours.Item1; g = colours.Item2; b = colours.Item3;
                     }
                     else
                     {
-                        float value = float.Clamp((PerlinMap[x, y] + 1) * 255 / 2, 0, 255);
+                        float value = float.Clamp((perlinMap[x, y] + 1) * 255 / 2, 0, 255);
                         byte colour = (byte)(value);
                         r = g = b = colour;
                     }
@@ -215,8 +282,21 @@ namespace ShortTools.Perlin
 
 
 
-        private static Tuple<byte, byte, byte> GetColours(float value)
+        private static Tuple<byte, byte, byte> GetColours(int x, int y, float[,] PerlinMap)
         {
+            float value = PerlinMap[x, y];
+
+            if (
+                (value > 0 && GetTileGradient(x, y, PerlinMap) > 0.27f) ||
+                (value > 0.4f && GetTileGradient(x, y, PerlinMap) > 0.15f))
+            {
+                return new Tuple<byte, byte, byte>(
+                    (byte)(100),
+                    (byte)(100),
+                    (byte)(100)
+                    );
+            }
+
             if (value < 0f) // from -1 to 0 so just add one ig
             {
                 return new Tuple<byte, byte, byte>(
@@ -230,14 +310,64 @@ namespace ShortTools.Perlin
                 return new Tuple<byte, byte, byte>(200, 200, 20);
             }
             else
-            { // 0.1 to 1, thats 0.9 which is basically 0 - 1
-                return new Tuple<byte, byte, byte>(
-                    (byte)(10 + value * 10), 
-                    (byte)(150 + value * 50),
-                    (byte)(10 + value * 20)
-                    );
+            { // 0.1 to 1, thats 0.9 which is basically 0 - 1 : grass
+                if (treeMap?[x, y] > 0.1f)
+                {
+                    return new Tuple<byte, byte, byte>(
+                        (byte)(10 + value * 10),
+                        (byte)(100 + value * 50),
+                        (byte)(10 + value * 20)
+                        );
+                }
+                else
+                {
+                    return CheckForResources(x, y, value);
+                    
+                }
             }
         }
+
+
+        private static Tuple<byte, byte, byte> CheckForResources(int x, int y, float value)
+        {
+            foreach (ResourceMap resourceMap in resourceMaps)
+            {
+                float distance = MathF.Abs(x - (centre.Item1 / 2)) + MathF.Abs(y - (centre.Item2 / 2));
+                if (distance < resourceMap.minDist) { continue; }
+
+                if (resourceMap.map[x, y] > resourceMap.minValue) { return resourceMap.colour; }
+            }
+
+
+
+
+            return new Tuple<byte, byte, byte>(
+                        (byte)(10 + value * 10),
+                        (byte)(150 + value * 50),
+                        (byte)(10 + value * 20)
+                        );
+        }
+
+
+
+
+
+
+
+        private static float GetTileGradient(int x, int y, float[,] PerlinMap)
+        {
+            if (x == 0 || y == 0 || x == PerlinMap.GetLength(0) - 1 || y == PerlinMap.GetLength(1) - 1) { return 0f; }
+
+            float mainTile = PerlinMap[x, y];
+            
+            float dn = MathF.Abs(PerlinMap[x    , y - 1] - mainTile);
+            float de = MathF.Abs(PerlinMap[x + 1, y    ] - mainTile);
+            float ds = MathF.Abs(PerlinMap[x    , y + 1] - mainTile);
+            float dw = MathF.Abs(PerlinMap[x - 1, y    ] - mainTile);
+
+            return (dn + de + ds + dw);
+        }
+
 
 
 
